7799974818648d4d95fa788094f9f80cbae9d5bf
SalahEddin Thabet <108817342+SEDDINT@users.noreply.github.com>
2023-04-15 22:42:45 +0300

    ya rab commit :)

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-let navLinks = document.querySelectorAll("nav a");
 
-navLinks.forEach(function(link) {
-    link.addEventListener("click", function(event) {
-        event.preventDefault();
-        let target = document.querySelector(link.getAttribute("href"));
-        target.scrollIntoView({ behavior: 'smooth' });
+function scroll_effect(element){
+    element.forEach(function(link) {
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
+            let target = document.querySelector(link.getAttribute("href"));
+            target.scrollIntoView({ behavior: 'smooth' });
+        });
     });
-});
+}
+
+let navLinks = document.querySelectorAll("nav a");
+scroll_effect(navLinks)
