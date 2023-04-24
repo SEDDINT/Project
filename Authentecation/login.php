85869d94ede37b9ab09182c14c6498c0dc3d5d3d
SalahEddin Thabet <108817342+SEDDINT@users.noreply.github.com>
2023-04-24 09:57:46 +0300

    website layout 1st draft

--- Authentecation/login.php
+++ Authentecation/login.php
@@ -31,7 +31,49 @@
 	}
 		
 	mysqli_close($conn);
+
+	
 ?>
 </body>
 </html>
 
+<?php
+// Define the database credentials
+$host = 'localhost';
+$user = 'username';
+$pass = 'password';
+$dbname = 'database_name';
+
+// Connect to the database
+$conn = mysqli_connect($host, $user, $pass, $dbname);
+
+// Check if the connection was successful
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Retrieve the username and password from the form
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// Query the user table for the provided credentials
+$sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+$result = mysqli_query($conn, $sql);
+
+// Check if the query returned a result
+if (mysqli_num_rows($result) == 1) {
+    // Start a session and redirect to the dashboard page
+    session_start();
+    $_SESSION['username'] = $username;
+    header('Location: dashboard.php');
+    exit;
+} else {
+    // Display an error message and redirect to the login page
+    echo 'Invalid username or password';
+    header('Location: login.php');
+    exit;
+}
+
+// Close the database connection
+mysqli_close($conn);
+?>
