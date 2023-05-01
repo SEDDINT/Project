feeeca98d9e6c012cacc713f3cb3e593ed1d38fa
SalahEddin Thabet <108817342+SEDDINT@users.noreply.github.com>
2023-05-01 20:11:13 +0300

    bookings issue fixed / registration

--- Config/login.php
+++ Config/login.php
@@ -1,53 +1,79 @@
-<?PHP
+<?php
+	// Start session
+	session_start();
+
+	// Include database connection
 	include("../db_login.php");
+
+	// Check if form is submitted
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		
+		// Get username and password from form
+		$username = $_POST["username"];
+		$password = $_POST["password"];
+
+		// Retrieve user data from database
+		$sql = "SELECT * FROM user_account WHERE username = '$username'";
+		$result = $conn->query($sql);
+
+		// Check if user exists in database
+		if ($result->num_rows == 1) {
+			
+			// Get user data from database
+			$row = $result->fetch_assoc();
+			$user_id = $row["user_id"];
+			$hashed_password = $row["password"];
+
+			// Check if password is correct
+			if (password_verify($password, $hashed_password)) {
+				
+				// Password is correct, start session and redirect to dashboard
+				$_SESSION["user_id"] = $user_id;
+				header("Location: dashboard.php");
+				exit();
+
+			} else {
+				// Password is incorrect, show error message
+				$error_message = "Invalid password";
+
+			}
+
+		} else {
+			
+			// User does not exist, show error message
+			$error_message = "Invalid username";
+
+		}
+
+	}
+
 ?>
-<html>
+
+<!DOCTYPE html>
+<html lang="en">
 <head>
+	<meta charset="UTF-8">
 	<title>Login</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
- 
-	<!--Script Link  put befor end of </body> -->
-	<link rel="stylesheet" href="../src/CSS/style.css">
-	<script src="../src/JS/main.js" defer></script>
 </head>
 <body>
-	<center>
-		<br>
-		<br>
-		<br>
-		<h3>Login</h3>
-		<form method="post" action="login.php" style="width: 100vh;">
-					<div class="form-group">
-						<label for="exampleInputEmail1">Username: </label>
-						<input  class="form-control"  type="text" name="username" id="username" placeholder="Enter your Username" required>
-						<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-					</div>
-					<div class="form-group">
-						<label for="exampleInputPassword1">Password: </label>
-						<input class="form-control" type="password" name="password" id="password" placeholder="Enter your Password" required >
-					</div>
-					<br>
-					<button type="submit" class="btn btn-dark">Login</button>
-		</form>
-	</center>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
+	<h1>Login</h1>
+
+	<?php if (isset($error_message)): ?>
+		<p><?php echo $error_message; ?></p>
+	<?php endif; ?>
+
+	<form method="post" action="login.php">
+
+		<label for="username">Username:</label>
+		<input type="text" name="username" id="username" required>
+
+		<label for="password">Password:</label>
+		<input type="password" name="password" id="password" required>
+
+		<input type="submit" value="Login">
+
+	</form>
+
 </body>
-
-	<?php
-		if ($_SERVER["REQUEST_METHOD"] == "POST") {
-			$username = $_POST["username"];
-			$password = $_POST["password"];
-
-			$sql = "SELECT * FROM user_account WHERE username = '$username' AND password = '$password'";
-			$result = mysqli_query($conn, $sql);
-			if (mysqli_num_rows($result) > 0) {
-				// Login successful
-				// Redirect to user.php
-				header("Location: ../www/user.php");
-			} else {
-				// Login failed
-				echo "Invalid username or password";
-			}
-		}
-		?>
 </html>
--- www/user.php
+++ www/user.php
@@ -10,53 +10,56 @@
 <center>
 	<h1>Gym Classes</h1>
 
-		<?php
-        $host_ip = "127.0.0.1";
-        $username = "root"; 
-        $password = "salaheddin"; 
-        $database = "gym_db";
+    <?php
+include("../db_login.php");
 
-        $conn = mysqli_connect($host_ip, $username, $password, $database, "4306");
+if (!$conn) {
+    echo "Debugging errno: " . mysqli_connect_errno();
+    echo "<br>";
+    echo "Debugging error: " . mysqli_connect_error();
+    exit;
+}
 
-        if (!$conn) {
-            echo "Debugging errno: " . mysqli_connect_errno();
-            echo "<br>";
-            echo "Debugging error: " . mysqli_connect_error();
-            exit;
-        }
+$query = "SELECT * from classes";
 
-        $query = "SELECT * from classes";
+$result = mysqli_query($conn, $query);
 
-        $result = mysqli_query($conn, $query);
-        if ($result) {
-        echo '<div class="container">';
-        echo    '<table class="table table-striped table-dark">';
-        echo        '<thead>';
-        echo            '<tr>';
-        echo            '<th scope="col">#</th>';
-        echo            '<th scope="col">Trainer Name</th>';
-        echo            '<th scope="col">Start Date</th>';
-        echo            '<th scope="col">Current Trainees</th>';
-        echo            '<th scope="col">Max Trainees</th>';
-        echo            '</tr>';
-        echo        '</thead>';
-        echo        '<tbody>';
-                    while ($result_row = mysqli_fetch_row($result)) {
-                        echo "<tr>";
-                        echo "<td>$result_row[0] </td>";
-                        echo "<td>$result_row[1] </td>";
-                        echo "<td> $result_row[2] </td>";
-                        echo "<td>$result_row[3] </td>";
-                        echo "<td>$result_row[4] </td>";
-                        echo "</tr>";
-                    }
-        echo        '</tbody>';
-        echo    '</table>';
-        echo '</div>';
-        } else {
-            echo "Error: " . $query . "<br>" . mysqli_error($conn);
-        }
-    ?>
+if ($result) {
+    echo '<div class="container">';
+    echo    '<table class="table table-striped table-dark">';
+    echo        '<thead>';
+    echo            '<tr>';
+    echo            '<th scope="col">#</th>';
+    echo            '<th scope="col">Trainer Name</th>';
+    echo            '<th scope="col">Start Date</th>';
+    echo            '<th scope="col">Bookings</th>';
+    echo            '<th scope="col">Max Trainees</th>';
+    echo            '</tr>';
+    echo        '</thead>';
+    echo        '<tbody>';
+
+    while ($result_row = mysqli_fetch_row($result)) {
+        $class_id = $result_row[0];
+        $bookings_query = "SELECT COUNT(*) FROM bookings WHERE class_id = $class_id";
+        $bookings_result = mysqli_query($conn, $bookings_query);
+        $bookings_count = mysqli_fetch_row($bookings_result)[0];
+
+        echo "<tr>";
+        echo "<td>$result_row[0] </td>";
+        echo "<td>$result_row[1] </td>";
+        echo "<td> $result_row[2] </td>";
+        echo "<td>$bookings_count</td>";
+        echo "<td>$result_row[4] </td>";
+        echo "</tr>";
+    }
+
+    echo        '</tbody>';
+    echo    '</table>';
+    echo '</div>';
+} else {
+    echo "Error: " . $query . "<br>" . mysqli_error($conn);
+}
+?>
 	<form action="user.php" method="post">
         <div class="dropdown">
 		<select name="class" id="class">
@@ -78,6 +81,8 @@
 </center>
 	<?php
         
+    //
+
 		// If the user has registered for a class, update the database
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             // Get the selected class ID from the form data
@@ -89,12 +94,14 @@
             if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
             }
+
             try {
             // Update the counter for the selected class
             $sql = "UPDATE classes SET number_of_trainees = number_of_trainees + 1 WHERE class_id = " . $class_id;
             if ($conn->query($sql) === TRUE) {
                 mysqli_query($conn, "INSERT INTO bookings (class_id) VALUES ($class_id)");
                 echo "";
+
             } else {
                 echo "Error updating class counter: " . $conn->error;
             }
